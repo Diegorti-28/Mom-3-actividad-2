e73c1d4fab58a079e9a40b5d1199cbbe7826985e
Juan Esteban Martinez Ortiz <juanemartinez@ucompensar.edu.co>
2026-05-20 21:26:41 -0500

    primer metodo

---
+++ ActividadMetodos.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ActividadMetodos{
+    static Scanner sc = new Scanner(System.in);
+    
+    //Primer Método:
+    public static void main(String[] args){
+        System.out.print("Deja tu mensaje: ");
+        String mensaje = sc.nextLine();
+        System.out.print(mensaje);
+
+    }
+}
