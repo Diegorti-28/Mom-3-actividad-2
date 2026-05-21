a63c40a67394413fd1a8162c9825158e7ed6c5c9
Juan Esteban Martinez Ortiz <juanemartinez@ucompensar.edu.co>
2026-05-20 21:36:33 -0500

    Segundo Método

--- ActividadMetodos.java
+++ ActividadMetodos.java
@@ -8,6 +8,17 @@ public class ActividadMetodos{
         System.out.print("Deja tu mensaje: ");
         String mensaje = sc.nextLine();
         System.out.print(mensaje);
+    }
+    //Segundo Método
+    
+    public static void mostrarDiasSemana() {
+        System.out.println("Lunes");
+        System.out.println("Martes");
+        System.out.println("Miercoles");
+        System.out.println("Jueves");
+        System.out.println("Viernes");
+        System.out.println("Sabado");
+        System.out.println("Domingo");
 
     }
 }
